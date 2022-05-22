c6600e53f1ed8d0ba44710cb7fee7d69999f5f93
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-05-22 19:25:53 +0600

    Initial Commit

---
+++ ExpenseTrackerII/ExpenseTrackerIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerIIApp.swift
+//  ExpenseTrackerII
+//
+//  Created by Sopnil Sohan on 22/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
